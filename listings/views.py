9867257bfb7f0cbd487885d5600e0599850f8f84
LostLite <williammuriuki@gmail.com>
2020-05-13 21:01:28 +0300

    Added single listing and pagination

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Listing
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'listings':page_obj
+    }
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = Listing.objects.get(id=listing_id)
+    context = {
+        'listing':listing,
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
